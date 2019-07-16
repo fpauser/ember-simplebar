3b66acbfdbe878dbdbe1134a8667013597dbd766
Falk Pauser <falk.pauser@voestalpine.com>
2019-07-16 13:13:19 +0200

    Update ember to 3.11.1 and ember-cli to 3.11.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,7 @@ module.exports = {
       files: ['.eslintrc.js', '.template-lintrc.js', 'ember-cli-build.js', 'index.js', 'testem.js', 'blueprints/*/index.js', 'config/**/*.js', 'tests/dummy/config/**/*.js'],
       excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
