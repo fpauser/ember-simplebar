e785ebb9eb8c244d3a5baa086a20af8eb40901f4
amelichow@voestalpine.com <artyom.melichow@voestalpine.com>
2020-02-28 16:09:16 +0100

    moved component styles to styles folder

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
     app.import('node_modules/simplebar/dist/simplebar.css');
-    app.import('vendor/simple-bar.css');
   }
 };
