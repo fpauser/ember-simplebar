97f016dab1f5348a28c57480efe2188c78ae25c0
Falk Pauser <falk.pauser@voestalpine.com>
2020-03-06 12:45:57 +0100

    Remove unneeded `tracked`

--- addon/components/simple-bar.js
+++ addon/components/simple-bar.js
@@ -1,9 +1,7 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
 export default class SimpleBarComponent extends Component {
-  @tracked
   instance = undefined;
 
   @action
