b50ffb9e3c19cd70acd035a1f7b5cdd7fd121c7b
Artyom Melichow <33684047+m3l1x@users.noreply.github.com>
2023-05-09 21:13:36 +0200

    Ember v4.12.1, Simplebar Core v1.2.4 (#17)
    Co-authored-by: Artyom Melichow <artyom.melichow@voestalpine.com>

--- addon/components/simple-bar.js
+++ addon/components/simple-bar.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import SimpleBarCore from 'simplebar-core';
 
 export default class SimpleBarComponent extends Component {
   @tracked
   instance = undefined;
+
+  get ariaLabel() {
+    return this.args.ariaLabel || SimpleBarCore.defaultOptions.ariaLabel;
+  }
 
   @action
   setInstance(instance) {
